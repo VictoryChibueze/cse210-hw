02e810f2580ae11ffe73dcd9a9158a2488521118
VictoryChibueze <alhajivicci12@gmail.com>
2024-01-08 19:47:12 +0000

    Variable

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompting the user for his/her first name
+        Console.Write("What is your first name? ");
+        // Saving the first name of the user in a variable
+        string fname = Console.ReadLine();
+
+        // Prompting the usre for his/her last name 
+        Console.Write("What is your last name? ");
+        // Saving the last name of the user in a variable
+        string lname = Console.ReadLine();
+
+        Console.Write(" ");
+
+        // Displaying the fullname of the user
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}");
+
+
+
+
     }
 }
