b7cca8a24dc805534d37a52805a5283b0d205f1f
VictoryChibueze <alhajivicci12@gmail.com>
2024-02-07 04:45:42 +0000

    week04 learning

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assigment = new Assignment("Samuel Burnet","Multiplication");
+        Console.WriteLine(assigment.GetSummary());
+        Console.WriteLine("");
+
+        MathAssignment assigment2 = new MathAssignment("Samuel Burnet","Fraction","Section 7.3","Problems 8-19");
+        Console.WriteLine(assigment2.GetSummary());
+        Console.WriteLine(assigment2.GetHomeworkList());
+
+        Console.WriteLine("");
+
+        WritingAssignment assigment3 = new WritingAssignment("Felix Navizdad","African Culture","The evolution of black Nok culture");
+
+        Console.WriteLine(assigment3.GetSummary());
+        Console.WriteLine(assigment3.GetWritingInformation());
     }
 }
