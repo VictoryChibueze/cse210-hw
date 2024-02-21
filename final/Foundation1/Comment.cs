087db683c2f80f7bb53787762e4a929becafd105
VictoryChibueze <alhajivicci12@gmail.com>
2024-02-21 12:27:50 +0000

    Foundation4

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,12 @@
+class public Comment{
+    public string _name;
+    public string _text;
+
+
+    public Comment(string name,string text)
+    {
+        _name = name;
+        _text = text;
+
+    }
+}
