e818aff49dfd2b126566e2410d1411c9658dc827
VictoryChibueze <alhajivicci12@gmail.com>
2024-02-22 21:41:52 +0000

    foundation3

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,35 @@
+public class Event
+{
+    private string _title;
+    private string _description;
+    private DateTime _date;
+    private string _address;
+
+
+    public Event(string title,string description,DateTime date,string address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _address = address;
+        date.
+
+    }
+
+    public string GetStandardDetails()
+    {
+        return $"Title: {_title}\n Description: {_description}\n Date: {_date.ToShortDateString()}\n Time: {_date.ToShortTimeString()}\nAddress:{_address}"
+
+    }
+
+    public string FullDetails()
+    {
+        return GetStandardDetails();
+
+    }
+    public string ShortDescription()
+    {
+        return $"Event Type: Generic Event \n Title: {_title} \n Date: {_date.ToShortDateString()}";
+    }
+
+}
