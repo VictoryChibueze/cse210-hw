ad5bee0badaf63d488a567309cb940eea4311464
VictoryChibueze <alhajivicci12@gmail.com>
2024-02-22 10:32:43 +0000

    foundation2

---
+++ final/Foundation2/Customer.cs
@@ -0,0 +1,14 @@
+public class Customer
+{
+    private string _name;
+    private string _customerAddress;
+
+    public Customer(string name , string customerAddress)
+    {
+        _name = name;
+        _customerAddress = customerAddress;
+    }
+
+    
+
+}
